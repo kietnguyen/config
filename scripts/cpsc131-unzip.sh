cfdf17485b81a89cc412039952be469a7e5feb55
Kiet Nguyen <kietnguyen86@gmail.com>
2015-04-14 22:29:01 -0700

    Edit to handle zip only

--- scripts/cpsc131-unzip.sh
+++ scripts/cpsc131-unzip.sh
@@ -16,20 +16,31 @@ echo "Extracting... $BASE_FILE" &>> $LOG_FILE
 mkdir -p zip-temp
 unzip "$BASE_FILE" -d zip-temp &>> $LOG_FILE
 
-for ZIP_FILE in zip-temp/*.zip; do
+for ZIP_FILE in zip-temp/*; do
   NAME=$(echo "$ZIP_FILE" | cut -d '/' -f 2 | cut -d '_' -f 1)
 
   echo "Create directory... $NAME" &>> $LOG_FILE
   mkdir -p "$NAME"
 
-  echo "Extracting... $ZIP_FILE" &>> $LOG_FILE
-  rm -rf cpp-temp && mkdir cpp-temp
-  unzip "$ZIP_FILE" -d cpp-temp &>> $LOG_FILE
+  FILE_EXT="${ZIP_FILE##*.}"
+  shopt -s nocasematch
+  if [ "$FILE_EXT" != "zip" ]; then
+    echo "####################" &>> $LOG_FILE
+    echo "$ZIP_FILE" &>> $LOG_FILE
+    echo "####################" &>> $LOG_FILE
+    continue;
 
-  echo "Moving files..." &>> $LOG_FILE
-  for CPP_FILE in $(find cpp-temp -regextype posix-egrep -iregex '[^.]*\.(h|cpp|txt|pdf|jpg|png)'); do
-    mv -v "$CPP_FILE" "$NAME" &>> $LOG_FILE
-  done
+    echo "Extracting... $ZIP_FILE" &>> $LOG_FILE
+    rm -rf file-temp && mkdir file-temp
+    unzip "$ZIP_FILE" -d file-temp &>> $LOG_FILE
+
+    echo "Moving files..." &>> $LOG_FILE
+    for FILE in $(find file-temp -regextype posix-egrep -iregex '[^\W]*\.(h|cpp|py|txt|pdf|doc|docx|odt|jpg|png)$'); do
+      mv -v "$FILE" "$NAME" &>> $LOG_FILE
+    done
+  fi
+  shopt -u nocasematch
+
 done
 
 echo "Clean up temp folder..." &>> $LOG_FILE
