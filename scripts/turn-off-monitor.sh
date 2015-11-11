376625bf7e31d637858bd36f2c2acd3191b531d3
Kiet Nguyen <kietnguyen86@gmail.com>
2015-11-10 22:34:47 -0800

    Update to lock screen before turning off monitor

--- scripts/turn-off-monitor.sh
+++ scripts/turn-off-monitor.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sleep 1; xset dpms force off
+sed -i 's/mode:\t\t.*/mode:\t\tblank/' $HOME/.xscreensaver \
+                                `# switch screensave mode to blank`   && \
+    xscreensaver-command -lock  `# lock screen`                       && \
+    sleep 1                     `# 1s timeout`                        && \
+    xset dpms force off         `# turn off monitor`
