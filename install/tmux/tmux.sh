46edd3028cbcae8d5b2b293c457f5a960cdc7a32
Kiet Nguyen <kietnguyen86@gmail.com>
2016-11-15 00:06:39 +0700

    Update tmux config files

--- install/tmux/tmux.sh
+++ install/tmux/tmux.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-SRC_DIR=$(cd $(dirname $BASH_SOURCE) && pwd)
+SRC_DIR=$(cd $(dirname "${BASH_SOURCE}") && pwd)
 
 # Install and copy config file
 sudo apt-get install -y tmux xclip
 
+# Install TMUX Plugin Manager (TPM)
+# See: https://github.com/tmux-plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "${HOME}"/.tmux/plugins/tpm
+
 # Copy config files
-ln -s ${SRC_DIR}/.tmux.conf ~/
+#ln -s "${SRC_DIR}"/.tmux.conf "${HOME}"
+
+# Reload TMUX environment
+tmux source "${HOME}"/.tmux.conf
